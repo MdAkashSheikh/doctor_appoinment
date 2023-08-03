d7fdbfbf0e1b48168523659fc9ebb254a2c1b246
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-08-03 10:41:49 +0600

    chamber-modify---width-full

--- admin/pages/pages/chamber-manage/index.js
+++ admin/pages/pages/chamber-manage/index.js
@@ -12,8 +12,8 @@ import { ProductService } from '../../../demo/service/ProductService';
 
 const Chamber_Manage = () => {
     let emptyProduct = {
-        st_time: '',
-        en_time: '',
+        id: 0,
+        chamber: '',
     };
 
     const [products, setProducts] = useState(null);
@@ -24,7 +24,6 @@ const Chamber_Manage = () => {
     const [selectedProducts, setSelectedProducts] = useState(null);
     const [submitted, setSubmitted] = useState(false);
     const [globalFilter, setGlobalFilter] = useState(null);
-    const [file, setFile] = useState();
     const toast = useRef(null);
     const dt = useRef(null);
     const [toggleRefresh, setTogleRefresh] = useState(false);
@@ -32,7 +31,7 @@ const Chamber_Manage = () => {
 
     useEffect(() => {
         ProductService.getProducts().then((data) => setProducts(data));
-    }, [toggleRefresh]);
+    }, []);
 
     const openNew = () => {
         setProduct(emptyProduct);
@@ -56,25 +55,18 @@ const Chamber_Manage = () => {
     const saveProduct = () => {
         setSubmitted(true);
 
-        console.log("PP1", product);
-        if((product.name && product.phone) && file ) {
+        console.log("PPPP1",product)
+
+        if( product.chamber) {
             ProductService.postProducts(
-                product.name, 
-                product.phone, 
-                product.email, 
-                product.company, 
-                product.designation, 
-                product.interest, 
-                product.companyType, 
-                product.comm, 
-                file
-            ).then(()=>{
-                setTogleRefresh(!toggleRefresh)
+
+                product.chamber,
+
+            ).then(() => {
+                setTogleRefresh(!toggleRefresh);
                 setProductDialog(false);
-            });
-    
+            })
         }
-        
     };
 
     const editProduct = (product) => {
@@ -88,65 +80,46 @@ const Chamber_Manage = () => {
     };
 
     const deleteProduct = () => {
-        ProductService.deleteProduct(product._id).then(()=>{
-            setTogleRefresh(!toggleRefresh);
-            setDeleteProductDialog(false);
-            setProduct(emptyProduct);
-            toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-        });
-
-        
-    };
-
-
-    const deleteSelectedProducts = () => {
-        let _products = products.filter((val) => !selectedProducts.includes(val));
+        let _products = products.filter((val) => val.id !== product.id);
         setProducts(_products);
-        setDeleteProductsDialog(false);
-        setSelectedProducts(null);
-        toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+        setDeleteProductDialog(false);
+        setProduct(emptyProduct);
+        toast.current.show({ severity: 'success', summary: 'Successful', detail: 'Time Deleted', life: 3000 });
     };
 
 
-    const onInputChange = (e) => {
+
+    const onInputChange = (e, name) => {
         const val = (e.target && e.target.value) || '';
         let _product = { ...product };
-        _product['name'] = val;
+        _product[`${name}`] = val;
 
         setProduct(_product);
     };
 
-    const leftToolbarTemplate = () => {
+
+    const codeBodyTemplate = (rowData) => {
         return (
             <>
-                <React.Fragment>
-                    <div className="my-2">
-                        <h2 className="m-0 p-column-title">Chamber Management</h2>
-                        {/* <Button label="New" icon="pi pi-plus" severity="sucess" className="mr-2" onClick={openNew} /> */}
-                    </div>
-                </React.Fragment>
+                <span className="p-column-title">Code</span>
+                {rowData.id}
             </>
         );
     };
 
 
-    const nameBodyTemplate = (rowData) => {
-        return (
-            <>
-                <span className="p-column-title">Name</span>
-                {rowData.name}
-            </>
-        );
-    };
 
-    const phoneBodyTemplate = (rowData) => {
+    const chamberBodyTemplate = (rowData) => {
         return (
             <>
-                <span className="p-column-title">Phone</span>
-                {rowData.phone}
+                <span className="p-column-title">Chamber Name</span>
+                {rowData.chamber}
             </>
         );
-    };
+    }
+
+
+
 
     const actionBodyTemplate = (rowData) => {
         return (
@@ -157,11 +130,29 @@ const Chamber_Manage = () => {
         );
     };
 
+        
+    const topHeader = () => {
+        return (
+            <React.Fragment>
+                <div className="my-2">
+                    <h2 className="m-0">Chamber Management</h2>
+                </div>
+            </React.Fragment>
+        );
+    };
+
     const header = (
         <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
-            <Button label="New" icon="pi pi-plus" severity="sucess" className="mr-2" onClick={openNew} />
+            <Button
+                    label="Add Time"
+                    icon="pi pi-plus"
+                    severity="sucess"
+                    className="mr-2"
+                    onClick={openNew}
+                />
             <span className="block mt-2 md:mt-0 p-input-icon-left">
-
+                <i className="pi pi-search" />
+                <InputText type="search" onChange={(e) => setGlobalFilter(e.target.value)} placeholder="Search..." />
             </span>
         </div>
     );
@@ -178,43 +169,41 @@ const Chamber_Manage = () => {
             <Button label="Yes" icon="pi pi-check" text onClick={deleteProduct} />
         </>
     );
-    const deleteProductsDialogFooter = (
-        <>
-            <Button label="No" icon="pi pi-times" text onClick={hideDeleteProductsDialog} />
-            <Button label="Yes" icon="pi pi-check" text onClick={deleteSelectedProducts} />
-        </>
-    );
-    
 
     if(products == null) {
         return (
             <div className="card">
-            <div className="border-round border-1 surface-border p-4 surface-card">
-                <div className="flex mb-3">
-                    <Skeleton shape="circle" size="4rem" className="mr-2"></Skeleton>
-                    <div>
-                        <Skeleton width="10rem" className="mb-2"></Skeleton>
-                        <Skeleton width="5rem" className="mb-2"></Skeleton>
-                        <Skeleton height=".5rem"></Skeleton>
+                <div className="border-round border-1 surface-border p-4 surface-card">
+                    <div className="flex mb-3">
+                        <Skeleton shape="circle" size="4rem" className="mr-2"></Skeleton>
+                        <div>
+                            <Skeleton width="10rem" className="mb-2"></Skeleton>
+                            <Skeleton width="5rem" className="mb-2"></Skeleton>
+                            <Skeleton height=".5rem"></Skeleton>
+                        </div>
+                    </div>
+                    <Skeleton width="100%" height="500px"></Skeleton>
+                    <div className="flex justify-content-between mt-3">
+                        <Skeleton width="4rem" height="2rem"></Skeleton>
+                        <Skeleton width="4rem" height="2rem"></Skeleton>
                     </div>
                 </div>
-                <Skeleton width="100%" height="500px"></Skeleton>
-                <div className="flex justify-content-between mt-3">
-                    <Skeleton width="4rem" height="2rem"></Skeleton>
-                    <Skeleton width="4rem" height="2rem"></Skeleton>
-                </div>
             </div>
-        </div>
         )
     }
+   
+
+
 
     return (
         <div className="grid crud-demo">
-            <div className="col-14">
+            <div className="col-12">
                 <div className="card">
                     <Toast ref={toast} />
-                    <Toolbar className="mb-4" left={leftToolbarTemplate}></Toolbar>
-
+                    <Toolbar
+                        className="mb-4"
+                        left={topHeader}
+                    ></Toolbar>
                     <DataTable
                         ref={dt}
                         value={products}
@@ -228,48 +217,50 @@ const Chamber_Manage = () => {
                         paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                         currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
                         globalFilter={globalFilter}
-                        emptyMessage="No products found."
+                        emptyMessage="Not found."
                         header={header}
                         responsiveLayout="scroll"
                     >
-                        
-                        <Column 
-                            field="name" 
-                            header="Chamber Name"  
-                            body={nameBodyTemplate} 
-                            headerStyle={{ minWidth: '15rem' }}
-                        >
-                
-                        </Column>
-                        <Column 
-                            header="Action" 
-                            body={actionBodyTemplate} 
-                            headerStyle={{ minWidth: '10rem' }}
-                        >
-                        </Column>
+
+                        <Column
+                            field="sl"
+                            header="SL"
+                            body={codeBodyTemplate}
+                            sortable
+                        ></Column>
+                         <Column
+                            field="chamber"
+                            header="Chamber Name"
+                            body={chamberBodyTemplate}
+                            headerStyle={{ minWidth: "20rem" }}
+                        ></Column>
+                        <Column
+                            header="Action"
+                            body={actionBodyTemplate}
+                            headerStyle={{ minWidth: "10rem" }}
+                        ></Column>
                     </DataTable>
 
-                    <Dialog 
-                        visible={productDialog} 
-                        style={{ width: '450px' }} 
-                        header="Add New Chamber" 
-                        modal 
-                        className="p-fluid" 
-                        footer={productDialogFooter} 
+                    <Dialog
+                        visible={productDialog}
+                        style={{ width: "450px" }}
+                        header="Add New Chamber "
+                        modal
+                        className="p-fluid"
+                        footer={productDialogFooter}
                         onHide={hideDialog}
                     >
-            
                         <div className="field">
-                            <label htmlFor="name">Chamber Name</label>
+                            <label htmlFor="chamber">Chamber Name</label>
                             <InputText 
-                                id="name" 
-                                value={product.name} 
-                                onChange={(e) => onInputChange(e)} 
-                                required autoFocus 
-                                className={classNames({ 'p-invalid': submitted && !product.name })} 
+                                id="chamber" 
+                                value={product.chamber} 
+                                onChange={(e) => onInputChange(e, "chamber")} 
+                                required 
+                                className={classNames({ 'p-invalid': submitted && !product.chamber })} 
                                 />
-                            {submitted && !product.name && <small className="p-invalid">
-                                Name is required.
+                            {submitted && !product.chamber && <small className="p-invalid">
+                                Chamber is required.
                             </small>}
                         </div>
                     </Dialog>
@@ -284,17 +275,11 @@ const Chamber_Manage = () => {
                             )}
                         </div>
                     </Dialog>
-
-                    <Dialog visible={deleteProductsDialog} style={{ width: '450px' }} header="Confirm" modal footer={deleteProductsDialogFooter} onHide={hideDeleteProductsDialog}>
-                        <div className="flex align-items-center justify-content-center">
-                            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
-                            {product && <span>Are you sure you want to delete the selected products?</span>}
-                        </div>
-                    </Dialog>
+                    
                 </div>
             </div>
         </div>
     );
 };
 
-export default Chamber_Manage;
+export default  Chamber_Manage;
